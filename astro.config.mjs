ccf012977641f316e487c3d1854a638dcdf85b96
Sandeep-FED <sandeep.ps0124@outlook.com>
2026-04-21 08:17:16 +0530

    refactor: add sitemap integration, update layout with SEO metadata, and create robots.txt for better indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Sandeep-FED.github.io",
   base: "/awesome-sharepoint",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: { plugins: [tailwindcss()] },
 })
